8c31d3faccc51cebb0041ae14cac0309031c593a
Justine Chu <justinegeechu333@gmail.com>
2024-02-04 20:06:01 -0600

    init

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <main className='flex flex-col gap-4 max-w-xl mx-auto bg-slate-500'>
+            <h1 className='w-full text-6xl'>Justine chu</h1>
+        </main>
+    );
+}
+
+export default Home;
