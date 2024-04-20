bc0b100eee1980b50af6ab8114ddc590c47f252d
Justine Chu <justinegeechu333@gmail.com>
2024-04-19 20:05:50 -0500

    added work info

--- src/components/TechIcon.jsx
+++ src/components/TechIcon.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./TechIcon.css";
+import classNames from "classnames";
 
-const TechIcon = ({ name }) => {
-    const path = name.includes('.') ? `/tech-stacks/${name}` : `/tech-stacks/${name}.svg`;
-    const label = name.includes('.') ? name.split('.')[0] : name;
-    return (
-        <div className='w-12 h-12 rounded-full shadow-md bg-slate-200 p-2 border border-slate-300' data-component="tech-icon">
-            <img src={path} alt={label} />
-        </div>
-    );
+const TechIcon = ({ name, isSmall, hoverInfo }) => {
+  const path = name.includes(".")
+    ? `/tech-stacks/${name}`
+    : `/tech-stacks/${name}.svg`;
+  const label = name.includes(".") ? name.split(".")[0] : name;
+  const props = hoverInfo ? { "data-hover-info": hoverInfo } : {};
+  return (
+    <div
+      className={classNames(
+        "rounded-full shadow-md bg-slate-200 border border-slate-300",
+        isSmall ? "w-8 h-8 p-1" : "w-12 h-12 p-2"
+      )}
+      data-component="tech-icon"
+      {...props}
+    >
+      <img src={path} alt={label} />
+    </div>
+  );
 };
 
 export default TechIcon;
--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -2,24 +2,31 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import "./Works.css";
 import { works } from "../../consts";
+import TechIcon from "../../components/TechIcon";
 
 const Works = () => {
   const { state } = useLocation();
   const { w } = state ?? {};
   const filterFunc = w ? ({ name }) => name === w : () => true;
   return (
-    <section data-page="work" className="flex flex-col gap-8 max-w-5xl mx-auto">
+    <section
+      data-page="work"
+      className="flex flex-col gap-8 max-w-6xl w-fit mx-auto"
+    >
       <section data-section="works">
         <h2 className="p-4">Works</h2>
-        <section className="grid grid-cols-12 gap-12 mx-auto max-w-6xl mt-8">
-          {works.filter(filterFunc).map(({ name, image }) => (
-            <AWork
-              key={name}
-              workName={name}
-              image={image}
-              className="col-span-12 md:col-span-6 lg:col-span-4"
-            />
-          ))}
+        <section className="flex flex-col gap-4">
+          {works
+            .filter(filterFunc)
+            .map(({ name, image, techStacks, description }) => (
+              <AWork
+                key={name}
+                workName={name}
+                image={image}
+                techStacks={techStacks}
+                description={description}
+              />
+            ))}
         </section>
       </section>
     </section>
@@ -28,20 +35,35 @@ const Works = () => {
 
 export default Works;
 
-const AWork = ({ workName, image, className }) => {
+const AWork = ({ workName, image, className, description, techStacks }) => {
   return (
     <div className={className}>
       <section
         data-work={workName}
-        className="flex flex-col max-w-xl mx-auto"
+        className="flex flex-row max-w-[800px] gap-4"
         data-section="home-work"
         data-item={workName}
-        data-techstack="dsjlfsdjlfdsjlf sdjfklsd sdjlfksdfsj"
+        data-techstack={techStacks.join(",")}
       >
-        <picture>
+        <picture className="basis-60 shrink-0 grow-0">
           <img src={image} alt={workName} />
           <caption about={workName} />
         </picture>
+        <section className="flex flex-col gap-2 flex-grow">
+          <h2>{workName}</h2>
+          <h3 className="text-lg">Tech Stacks</h3>
+          <p className="text-sm">{description}</p>
+          <div className="flex flex-row gap-2 flex-wrap">
+            {techStacks.map((skill) => (
+              <TechIcon
+                isSmall={true}
+                key={skill}
+                name={skill}
+                hoverInfo={skill}
+              />
+            ))}
+          </div>
+        </section>
       </section>
     </div>
   );
